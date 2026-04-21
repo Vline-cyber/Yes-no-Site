6fd35ec1b12eb89f598ef151713b809a0e8abc95
Vline <dmitry.vasvas.roman9@gmail.com>
2026-04-21 12:34:50 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const questionInput = document.getElementById('questionInput');
+const askButton = document.getElementById('askButton');
+const answerGif = document.getElementById('answerGif');
+
+askButton.addEventListener('click', function() {
+    const question = questionInput.value;
+    if (!question) {
+        alert('Пожалуйста, введите вопрос');
+        return;
+    }
+
+    fetch('https://yesno.wtf/api')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const answer = data.answer;
+            answerGif.scr = data.image;
+            answerGif.style.display = 'block';
+            questionInput.style.display = 'none';
+            askButton.style.display = 'none';
+        })
+        .catch(error => {
+            console.error('Ошибка при запросе к Api');
+            alert('Не удалось получить ответ. попробуйте ещё раз');
+        });
+});
